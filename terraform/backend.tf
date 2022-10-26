0f926c381aef7315a274afc61503c3189e46dcb7
pablofischer <pablo.fischer.nl@leiainc.com>
2022-10-26 14:19:59 -0300

    fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 0.13.5"
 
   backend "s3" {
-    region = "us-east-1"
-    bucket = "devops-challenge-pf"
+    region   = "us-east-1"
+    bucket   = "devops-challenge-pf"
     profile  = ""
     role_arn = ""
     encrypt  = "true"
